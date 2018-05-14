11893c8112d5bd6c270e6202a3a77ab689d4cb06
fanlun007 <13058100289@163.com>
2018-05-14 18:14:13 +0800

    idea&git

---
+++ src/main/java/com/fanlun/springboot/controller/HelloController.java
@@ -0,0 +1,30 @@
+package com.fanlun.springboot.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+@Controller
+public class HelloController {
+
+    @RequestMapping("/hello")
+    public String hello(){
+        return "hello";
+    }
+
+    @RequestMapping("/success")
+    public String success(Map<String, Object> map){
+        List<String> users = new ArrayList<>();
+        users.add("fanlun100");
+        users.add("jiny");
+        users.add("huanyue");
+        map.put("hello", "fanlun");
+        map.put("users", users);
+
+        return "success";
+    }
+    //ijij
+}
